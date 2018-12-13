822fe0905ca81824c7fff2b068291c2a8dac6b29
Ryan Wahl <rwahl005@west-mec.org>
2018-12-13 14:15:07 -0700

    Updated Code
    Updated the coded. Added in comments into webpages. Formatted the index page to work with mobile devices. Commented javascript code.

--- js/generic_popup.js
+++ js/generic_popup.js
@@ -3,27 +3,34 @@
     Used to dynamicly create popup menus.
 */
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) { //Checks to see if the browser is mobile.
+    //Gets the html portion of the popup menu.
     var elm = document.getElementById("window");
 
+    //hides the menu by default
     elm.style.display = "none";
 
+    //A function to open the menu that is used in the inclick="" attribute
     function openmenu(title, p, header) {
         alert("That feature is disabled on mobile!");
     }
 } else {
+    //Gets the html portion of the popup menu.
     var elm = document.getElementById("window");
 
+    //hides the menu by default
     elm.style.display = "none";
 
-
+    //A function to open the menu that is used in the inclick="" attribute
     function openmenu(title, p, header) {
         elm.style.display = "block"; //Shows the menu.
+        //Grabs all of the elements to insert the text.
         elm.getElementsByTagName("p")[0].innerHTML = title;
         elm.getElementsByTagName("p")[1].innerHTML = p;
         elm.getElementsByTagName("h1")[0].innerHTML = header;
     }
 
+    //closes the menu
     function closemenu() {
         elm.style.display = "none"; //Hides it again.
         // Resets the menu position.
@@ -37,6 +44,7 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     //grabs the element and runs the function dragelement
 
     function dragElement(elmnt) {
+        //Stores the current and past positions
         var pos1 = 0,
             pos2 = 0,
             pos3 = 0,
@@ -46,30 +54,30 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
             // get the element with an id of header added to it. (S)
             document.getElementById(elm.id + "header").onmousedown = dragMouseDown;
         } else {
-            // otherwise, move the DIV from anywhere inside the DIV: 
+            // otherwise, move the DIV from anywhere inside the DIV
             elmnt.onmousedown = dragMouseDown;
         }
 
         function dragMouseDown(e) {
             e = e || window.event;
             e.preventDefault();
-            // get the mouse cursor position at startup:
+            // get the mouse cursor position at startup
             pos3 = e.clientX;
             pos4 = e.clientY;
             document.onmouseup = closeDragElement;
-            // call a function whenever the cursor moves:
+            // call a function whenever the cursor moves
             document.onmousemove = elementDrag;
         }
 
         function elementDrag(e) {
             e = e || window.event;
             e.preventDefault();
-            // calculate the new cursor position:
+            // calculate the new cursor position
             pos1 = pos3 - e.clientX;
             pos2 = pos4 - e.clientY;
             pos3 = e.clientX;
             pos4 = e.clientY;
-            // set the element's new position:
+            // set the element's new position
             if (!((elmnt.offsetTop - pos2) < 0))
                 elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
             if (!((elmnt.offsetLeft - pos1) < 0))
@@ -77,7 +85,7 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
         }
 
         function closeDragElement() {
-            // stop moving when mouse button is released:
+            // stop moving the menu when mouse button is released
             document.onmouseup = null;
             document.onmousemove = null;
         }
